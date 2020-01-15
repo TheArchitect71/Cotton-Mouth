2dad537442a356ac90d4d489da7e3f21e75999bd
Erick F <erickflores785@gmail.com>
2020-01-14 21:38:50 -0600

    Added more routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent} from './home/home.component';
-import { ExperienceComponent } from './experience/experience.component';
-import { TechnologyComponent } from './technology/technology.component';
 import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { CultureComponent } from './culture/culture.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { PhotographyComponent } from './photography/photography.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TechnologyComponent } from './technology/technology.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'about', component: AboutComponent },
+  { path: 'experience', component: ExperienceComponent},
+  { path: 'culture', component: CultureComponent},
+  { path: 'favorite', component: FavoritesComponent},
+  { path: 'photography', component: PhotographyComponent},
+  { path: 'projects', component: ProjectsComponent},
   { path: 'technology', component: TechnologyComponent},
-  { path: 'experience', component: ExperienceComponent}
 ];
 
 @NgModule({
