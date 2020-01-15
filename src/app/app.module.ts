844bda10a4323868477eaed38561e4b4432eb2c6
Erick F <erickflores785@gmail.com>
2020-01-14 21:55:20 -0600

    More Declartions!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { PhotographyComponent } from './photography/photography.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { CultureComponent } from './culture/culture.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +22,11 @@ import { HomeComponent } from './home/home.component';
     AboutComponent,
     NavigationComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    ProjectsComponent,
+    PhotographyComponent,
+    FavoritesComponent,
+    CultureComponent
   ],
   imports: [
     BrowserModule,
