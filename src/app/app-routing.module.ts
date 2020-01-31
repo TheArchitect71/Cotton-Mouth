d6481eec835a14047650d8dd7e6d08612ec1df84
Erick F <erickflores785@gmail.com>
2020-01-31 12:16:37 -0600

    new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent} from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ExperienceComponent } from './experience/experience.component';
-import { CultureComponent } from './culture/culture.component';
 import { FavoritesComponent } from './favorites/favorites.component';
-import { PhotographyComponent } from './photography/photography.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { TechnologyComponent } from './technology/technology.component';
 
@@ -14,9 +12,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'about', component: AboutComponent },
   { path: 'experience', component: ExperienceComponent},
-  { path: 'culture', component: CultureComponent},
   { path: 'favorites', component: FavoritesComponent},
-  { path: 'photography', component: PhotographyComponent},
   { path: 'projects', component: ProjectsComponent},
   { path: 'technology', component: TechnologyComponent},
 ];
