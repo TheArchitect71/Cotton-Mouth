f94826d7f5055910d906999d25a87367dbb35c6f
Erick Flores <thearchitect@MacBook-Air.local>
2020-06-18 21:49:48 -0500

    less HTML!

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -10,17 +10,32 @@ import { Information } from '../information';
 })
 export class AboutComponent {
 
-  documents: Information[];
   constructor() {}
 
-  // constructor(private starLinkService: StarLinkService) {}
-
-  // getDocuments(): void {
-  //   this.starLinkService.getDocuments()
-  //     .subscribe(documents => this.documents = documents);
-  // }
-  // ngOnInit() {
-  //   this.getDocuments();
-  // }
+  summaries = [
+    {
+      id:1,
+      text: `As a web developer, I work to better understand how web applications
+      motivates and shapes the way we do business and administration. My
+      expertise includes project design and management, data manipulation and
+      interpretation, and the development and implementation of open-source
+      tools.`
+    },
+    {
+      id:2,
+      text: `I enjoy generating new ideas and devising feasible solutions to broadly
+      relevant problems. My colleagues would describe me as a driven,
+      resourceful individual who maintains a positive, proactive attitude when
+      faced with adversity.`
+    },
+    {
+      id:3,
+      text: `Currently, I’m seeking opportunities that will allow me to develop and
+      promote technologies that benefit the development of the humanities.
+      Specific fields of interest include machine learning, biotechnology, and
+      internet-of-things. 🕸`
+    }
+  ]
+ 
 
 }
