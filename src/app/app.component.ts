8f512d0b56d8d535693c0bb93388da400984e78e
Erick Flores <thearchitect@MacBook-Air.local>
2020-07-08 21:11:02 -0500

    no header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { profile } from "console";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  
+  opened: boolean;
+
+  navigationList = [
+    {
+      id: 2,
+      route: "/experience",
+      title: `Services`,
+      content: `Learning is an Active Process that Requires Ardor and Diligence.`,
+    },
+    {
+      id: 3,
+      route: "/technology",
+      title: `Small Business`,
+      content: `We apply the industry best practices to help you succeed in your business. We involve you in the process to find out the best possible solution.`,
+    },
+    {
+      id: 6,
+      route: "/favorites",
+      title: `Education`,
+      content: `Facilitating the acquisition of knowledge, skills, values, beliefs, and habits.`,
+    },
+    {
+      id: 1,
+      route: "/about",
+      title: `About`,
+      content: `To Be Authentic. To Surrender the Outcome. To Do Uncomfortable Work.`,
+    },
+  ];
 }
