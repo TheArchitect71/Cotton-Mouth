e46695f2c9b1690e83c76003ee89fa8051ca4562
Erick Flores <thearchitect@MacBook-Air.local>
2020-08-09 00:59:09 -0500

    The about we deserve

--- src/app/app-shell/about/about.component.ts
+++ src/app/app-shell/about/about.component.ts
@@ -33,6 +33,46 @@ export class AboutComponent {
       internet-of-things. 🕸`
     }
   ]
+
+  navigationList = [
+    {
+      id: 1,
+      route: '/about',
+      title: `About`,
+      content: `To Be Authentic. To Surrender the Outcome. To Do Uncomfortable Work.`
+    },
+    {
+      id: 2,
+      route: '/experience',
+      title: `Experience`,
+      content: `Learning is an Active Process that Requires Ardor and Diligence.`
+    },
+    {
+      id: 3,
+      route: '/projects',
+      title: `Projects`,
+      content: `Making a Lively Game Out of Mastering Web Development.`
+    },
+    {
+      id: 4,
+      route: '/technology',
+      title: `Technology`,
+      content: `Developing the Front-End and Back-End of the Web using Open Source
+      Tools.`
+    },
+    {
+      id: 5,
+      route: '/favorites',
+      title: `Favorites`,
+      content: `Books, Movies and TV Shows Dealing with the Humanities.`
+    },
+    // {
+    //   id: 6,
+    //   route: '/education',
+    //   title: `Education`,
+    //   content: `Facilitating the acquisition of knowledge, skills, values, beliefs, and habits.`
+    // }
+  ]
  
 
 }
