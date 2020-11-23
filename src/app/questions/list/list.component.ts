c02f5762acc357ebe369ec5f8731107a7ddc633a
Erick Flores <thearchitect@MacBook-Air.local>
2020-11-23 12:59:50 -0600

    data populated!

--- src/app/questions/list/list.component.ts
+++ src/app/questions/list/list.component.ts
@@ -1,24 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import { QuestionsService } from '../questions.service';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { QuestionsService } from "../questions.service";
+import { PageEvent } from "@angular/material/paginator";
+import { Subscription } from "rxjs";
+import { AuthService } from "src/app/authentication/auth.service";
+import { Question } from "../question.model";
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  selector: "app-list",
+  templateUrl: "./list.component.html",
+  styleUrls: ["./list.component.css"],
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, OnDestroy {
   panelOpenState = false;
   questions = [];
-  constructor(private questionsService: QuestionsService) { }
+  isLoading = false;
+  totalQuestions = 0;
+  questionsPerPage = 2;
+  currentPage = 1;
+  pageSizeOptions = [1, 2, 5, 10];
+  userIsAuthenticated = false;
+  userId: string;
+  private questionsSub: Subscription;
+  private authStatusSub: Subscription;
+
+  constructor(
+    private questionsService: QuestionsService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
-    this.getQuestions
+    this.questionsService.getQuestions(this.questionsPerPage, this.currentPage);
+    this.userId = this.authService.getUserId();
+    this.questionsSub = this.questionsService
+      .getQuestionUpdateListener()
+      .subscribe(
+        (questionData: { questions: Question[]; questionCount: number }) => {
+          this.isLoading = false;
+          this.totalQuestions = questionData.questionCount;
+          this.questions = questionData.questions;
+        }
+      );
+  }
+  
+  onChangedPage(pageData: PageEvent) {
+    this.isLoading = true;
+    this.currentPage = pageData.pageIndex + 1;
+    this.questionsPerPage = pageData.pageSize;
+    this.questionsService.getQuestions(this.questionsPerPage, this.currentPage);
   }
 
-  getQuestions(): void {
-    this.questionsService.getQuestions().subscribe(questions => {
-      this.questions = questions;
-    })
+  // getQuestions(): void {
+  //   this.questionsService.getQuestions().subscribe((questions) => {
+  //     this.questions = questions;
+  //   });
+  // }
+  ngOnDestroy() {
+    this.questionsSub.unsubscribe();
+    this.authStatusSub.unsubscribe();
   }
-
 }
