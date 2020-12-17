4c364e66a5fa2b60388972d840ad579c4538c078
Erick Flores <erickflores785@gmail.com>
2020-12-16 22:35:24 -0600

    responsive sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, ChangeDetectorRef, OnDestroy } from "@angular/core";
 import { Subscription } from "rxjs";
+import {MediaMatcher} from '@angular/cdk/layout';
 import { AuthService } from "src/app/authentication/auth.service";
 
 @Component({
@@ -8,18 +9,28 @@ import { AuthService } from "src/app/authentication/auth.service";
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  mobileQuery: MediaQueryList;
   opened: boolean;
   userIsAuthenticated = false;
+  private _mobileQueryListener: () => void;
   private authListenerSubs: Subscription;
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher
+  ) {
+    this.mobileQuery = media.matchMedia("(max-width: 600px)");
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
 
   ngOnInit() {
     this.authService.autoAuthUser();
     this.userIsAuthenticated = this.authService.getIsAuth();
     this.authListenerSubs = this.authService
       .getAuthStatusListener()
-      .subscribe(isAuthenticated => {
+      .subscribe((isAuthenticated) => {
         this.userIsAuthenticated = isAuthenticated;
       });
   }
@@ -30,6 +41,7 @@ export class AppComponent {
 
   ngOnDestroy() {
     this.authListenerSubs.unsubscribe();
+    this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 
   navigationList = [
@@ -40,10 +52,10 @@ export class AppComponent {
       content: `To Be Authentic. To Surrender the Outcome. To Do Uncomfortable Work.`,
     },
     {
-      title: `Contact Us`
+      title: `Contact Us`,
     },
     {
-      title: 'Log In'
-    }
+      title: "Log In",
+    },
   ];
 }
